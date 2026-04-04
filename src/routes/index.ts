f0812867657d6dd1684628bbe6f03367f6790488
Muhammad Zaki <zakybaraq@gmail.com>
2026-04-04 12:19:39 +0700

    fix: Move cookie plugin to routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from 'elysia';
+import { cookie } from '@elysiajs/cookie';
 import { menuRoutes } from './menus';
 import { tableRoutes } from './tables';
 import { orderRoutes } from './orders';
 import { authRoutes } from './auth';
 
 export const routes = new Elysia()
+  .use(cookie())
   .use(menuRoutes)
   .use(tableRoutes)
   .use(orderRoutes)
